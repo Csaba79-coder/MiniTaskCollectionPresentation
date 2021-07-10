64a12534a99b85dbb30e94bc739c731769e0e599
Csaba79-coder <csabavadasz79@gmail.com>
2021-07-10 18:21:58 +0200

    refaktor

--- src/java/model/DiceEnum.java
+++ src/java/model/DiceEnum.java
@@ -21,4 +21,20 @@ public enum DiceEnum {
     public void setNUM(int NUM) {
         this.NUM = NUM;
     }
+
+
+   /* public final String label;
+    public final int num;
+
+    MyEnum(String label, int num) {
+        this.label = label;
+        this.num = num;
+
+    public MyEnum getEnumByNum(int num) {
+        for (MyEnum myEnum : values()) {
+            if (myEnum.num == num)
+                return myEnum;
+        }
+        return null;
+    }*/
 }
